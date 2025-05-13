e6415c82f4c137ceb53906eaf90be1aa2690c46e
NguyenHo12 <nho4@ivytech.edu>
2025-05-13 19:40:30 +0000

    fix some bugs

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,41 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Post
-from .forms import CommentForm
-from .models import Comment
+from .forms import CommentForm, PostForm
+from django.utils import timezone
+from .models import Comment, Post
+
 
 @login_required
 def post_new(request):
-    [...]
-  
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
 def post_draft_list(request):
   posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
   return render(request, 'blog/post_draft_list.html', {'posts': posts})
@@ -41,13 +69,14 @@ def add_comment_to_post(request, pk):
       form = CommentForm()
   return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
+@login_required
 def comment_approve(request, pk):
-  comment = get_object_or_404(Comment, pk=pk)
-  comment.approve()
-  return redirect('post_detail', pk=comment.post.pk)
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
 
 @login_required
 def comment_remove(request, pk):
-  comment = get_object_or_404(Comment, pk=pk)
-  comment.delete()
-  return redirect('post_detail', pk=comment.post.pk)
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
